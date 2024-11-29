08633d2e3bcf79240f1f1460fe21a4e3db192054
initeen <135745505+initeen@users.noreply.github.com>
2024-11-29 13:04:02 +0530

    Update Logs

--- src/main/java/com/baxees/weddingduniya/WeddingDuniyaApplication.java
+++ src/main/java/com/baxees/weddingduniya/WeddingDuniyaApplication.java
@@ -8,7 +8,7 @@ public class WeddingDuniyaApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(WeddingDuniyaApplication.class, args);
-		System.out.println("Project start..");
+		System.out.println("Project start....");
 		
 
 	}
